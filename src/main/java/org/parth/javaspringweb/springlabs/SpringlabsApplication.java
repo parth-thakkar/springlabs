c50cc5a4e1f881b4aa849d3417203d83c6631759
Parth Thakkar <parth.techblogs@gmail.com>
2024-10-12 09:20:23 +0200

    first commit

---
+++ src/main/java/org/parth/javaspringweb/springlabs/SpringlabsApplication.java
@@ -0,0 +1,13 @@
+package org.parth.javaspringweb.springlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlabsApplication.class, args);
+	}
+
+}
